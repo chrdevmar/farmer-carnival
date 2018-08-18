24f201815cbb3340fa83d569381d571c9d243f8d
Andrew O'Rourke <andrew.john.orourke@gmail.com>
2018-08-18 16:17:47 +1000

    UDPATE: Updated balance handling on /commit route.

--- api/index.js
+++ api/index.js
@@ -8,28 +8,33 @@ var jwt = require('jsonwebtoken');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// User's balance
-var balance = 0;
-
 app.get('/', (req, res, next) => {
   res.send('Hello world');
 })
 
+// Route for commiting the user's donation balance & generating a JWT with this balance
 app.post('/commit', (req, res) => {
-  // Update user's balance
-  balance = req.body.balance;
-  // Generate JWT
-  var token = jwt.sign({
-    balance: balance
-  }, process.env.SECRET_KEY,
-  {
-    expiresIn: "2h"
-  })
-  // Send JWT
-  res.send(token);
+  // Check if body contains a balance
+  if(req.body.balance) {
+    // Generate JWT
+    var token = jwt.sign({
+      balance: req.body.balance
+    }, process.env.SECRET_KEY,
+    {
+      expiresIn: "2h"
+    })
+    // Send JWT
+    res.send(token);
+  } else {
+    res.status(500).send({
+      "error": "Invalid balance received. Balance must be a number and greater than 0."
+    });
+  }
 })
 
+app.post('/spend', (req, res) => {
 
+})
 
 app.listen(8000);
 console.log('listening on port 8000')
