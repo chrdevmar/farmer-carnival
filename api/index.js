ac13d2a674fde52f725a25e467a254a08d4c93cf
Andrew O'Rourke <andrew.john.orourke@gmail.com>
2018-08-18 16:02:14 +1000

    ADD: /commit route generates and returns a JWT token containing the user's balance.

--- api/index.js
+++ api/index.js
@@ -1,10 +1,35 @@
-const express = require('express');
+require('dotenv').config()
 
+const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+var jwt = require('jsonwebtoken');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// User's balance
+var balance = 0;
 
 app.get('/', (req, res, next) => {
-  res.send('Hello World');
+  res.send('Hello world');
 })
+
+app.post('/commit', (req, res) => {
+  // Update user's balance
+  balance = req.body.balance;
+  // Generate JWT
+  var token = jwt.sign({
+    balance: balance
+  }, process.env.SECRET_KEY,
+  {
+    expiresIn: "2h"
+  })
+  // Send JWT
+  res.send(token);
+})
+
+
 
 app.listen(8000);
 console.log('listening on port 8000')
